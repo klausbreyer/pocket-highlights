2edb4571b2d2491963dac2049c680dd1aea84279
kb0 <kb@v01.io>
2020-12-30 07:38:20 +0100

    added features

--- convert.js
+++ convert.js
@@ -6,7 +6,6 @@ const express = require('express')
 const object1 = input.list
 const filtered = []
 for (const [key, value] of Object.entries(object1)) {
-
     if (value.annotations) {
         filtered.push(value)
     }
@@ -19,21 +18,25 @@ const content = filtered.reverse().map(item => {
     if (item.authors) {
         for (const [key, author] of Object.entries(item.authors)) {
             authors.push(author.name)
+
         }
     }
+    const annotated = item.annotations[0].created_at;
     const annotations = item.annotations.map(annotation => `<li>${annotation.quote}`)
 
-    return `<h1>${item.resolved_title}${authors.length > 0 ? ', ' : ''}${authors.join(",")}, ${item.annotations[0].created_at.split("-").shift()}</h1><li>${item.resolved_url}</li>${annotations}</ul></ul><hr />\n`;
+    return `<h1>${item.resolved_title}${authors.length > 0 ? ', ' : ''}${authors.join(",")}, ${annotated.split("-").shift()}</h1><aside>${annotated}: ${item.excerpt} </aside><ul><li><a href="${item.resolved_url}" target="_blank">${item.resolved_url}</a></li>${annotations}</ul><hr />\n`;
 }).join("")
 
 
-fs.writeFileSync('highlights.html', content)
+const full = `<title>Pocket Highlights</title>${content}`;
+
+fs.writeFileSync('highlights.html', full)
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send(content)
+    res.send(full)
 })
 
 app.listen(port, () => {
