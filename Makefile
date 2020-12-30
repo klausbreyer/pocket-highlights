734c3e14d3ac2333e18ab9fe9b666c8649b06a4e
kb0 <kb@v01.io>
2020-12-30 07:26:07 +0100

    build

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-start: 
+export: 
 	python3 -m pockexport.export --secrets pocketsecrets.py > export.json
+
+convert:
+	node convert.js
+
+magic:
+	make export && make convert
--- convert.js
+++ convert.js
@@ -1,40 +1,39 @@
 const input = require('./export.json')
+const fs = require('fs')
 const express = require('express')
 
 
 const object1 = input.list
 const filtered = []
 for (const [key, value] of Object.entries(object1)) {
-    console.log(`${key}: ${value}`);
+
     if (value.annotations) {
         filtered.push(value)
     }
 }
 
-console.log(filtered)
 
+const content = filtered.reverse().map(item => {
+    //evtl. datum. evt. autor
+    const authors = [];
+    if (item.authors) {
+        for (const [key, author] of Object.entries(item.authors)) {
+            authors.push(author.name)
+        }
+    }
+    const annotations = item.annotations.map(annotation => `<li>${annotation.quote}`)
+
+    return `<h1>${item.resolved_title}${authors.length > 0 ? ', ' : ''}${authors.join(",")}, ${item.annotations[0].created_at.split("-").shift()}</h1><li>${item.resolved_url}</li>${annotations}</ul></ul><hr />\n`;
+}).join("")
+
+
+fs.writeFileSync('highlights.html', content)
 
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-
-    const result = filtered.map(item => {
-        //evtl. datum. evt. autor
-        const authors = [];
-        if (item.authors) {
-            for (const [key, author] of Object.entries(item.authors)) {
-                authors.push(author.name)
-            }
-        }
-        const annotations = item.annotations.map(annotation => `<li>${annotation.quote}`)
-
-        return `<h1>${item.resolved_title}${authors.length > 0 ? ', ' : ''}${authors.join(",")}, ${item.annotations[0].created_at.split("-").shift()}</h1><li>${item.resolved_url}</li>${annotations}</ul></ul><hr />`;
-    }).join("")
-
-
-
-    res.send(result)
+    res.send(content)
 })
 
 app.listen(port, () => {
