a27aafffd3a8fac33dc69db2f174026c116a5dcd
Klaus Breyer <kb@v01.io>
2021-01-01 15:27:57 +0100

    project hygiene, better sorting

--- convert.js
+++ convert.js
@@ -1,31 +1,52 @@
-const input = require('./export.json')
-const fs = require('fs')
+const input = require("./export.json");
+const fs = require("fs");
 
-
-const object1 = input.list
-const filtered = []
-for (const [key, value] of Object.entries(object1)) {
-    if (value.annotations) {
-        filtered.push(value)
-    }
+const fullList = input.list;
+const reducedList = [];
+for (const [key, value] of Object.entries(fullList)) {
+  if (value.annotations) {
+    reducedList.push(value);
+  }
 }
 
-
-const content = filtered.reverse().map(item => {
+const content = reducedList
+  .sort((a, b) => {
+    if (a.annotations[0].created_at > b.annotations[0].created_at) {
+      return -1;
+    }
+    if (a.annotations[0].created_at < b.annotations[0].created_at) {
+      return 1;
+    }
+    return 0;
+  })
+  .map((item) => {
     //evtl. datum. evt. autor
     const authors = [];
     if (item.authors) {
-        for (const [key, author] of Object.entries(item.authors)) {
-            authors.push(author.name)
-
-        }
+      for (const [key, author] of Object.entries(item.authors)) {
+        authors.push(author.name);
+      }
     }
     const annotated = item.annotations[0].created_at;
-    const annotations = item.annotations.map(annotation => `<li>${annotation.quote}`)
+    const annotations = item.annotations.map(
+      (annotation) => `<li>${annotation.quote}`
+    );
 
-    return `<h2>${item.resolved_title}${authors.length > 0 ? ', ' : ''}${authors.join(",")}, ${annotated.split("-").shift()}</h2><aside>${annotated}: ${item.excerpt} </aside><ul><li><a href="${item.resolved_url}" target="_blank">${item.resolved_url}</a></li>${annotations}</ul><hr />\n`;
-}).join("")
+    return `<h2>${item.resolved_title}${
+      authors.length > 0 ? ", " : ""
+    }${authors.join(",")}, ${annotated
+      .split("-")
+      .shift()}</h2><aside>${annotated}: ${
+      item.excerpt
+    } </aside><ul><li><a href="${item.resolved_url}" target="_blank">${
+      item.resolved_url
+    }</a></li>${annotations}</ul><hr />\n`;
+  })
+  .join("");
 
-
-
-fs.writeFileSync('dist/index.html', `${fs.readFileSync("theme/header.html")}${content}${fs.readFileSync("theme/footer.html")}`)
+fs.writeFileSync(
+  "dist/index.html",
+  `${fs.readFileSync("theme/header.html")}${content}${fs.readFileSync(
+    "theme/footer.html"
+  )}`
+);
